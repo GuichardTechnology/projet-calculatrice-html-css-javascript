be6161b8bfb9f16252f82dccafaf30d99f3841cb
GuichardTechnology <guichardj483@gmail.com>
2023-09-16 17:22:07 -0300

    calculatrice

---
+++ index.js
@@ -0,0 +1,49 @@
+
+function addition(){
+    let n1 = document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1);
+    let numero2 = parseFloat(n2);
+
+    let addition = numero1 + numero2
+
+    window.alert("le resultat de l additon est " + addition)
+}
+
+
+function soustraction(){
+    let n1 = document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1);
+    let numero2 = parseFloat(n2);
+
+    let soustraction = numero1 - numero2
+
+    window.alert("le resultat de la soustraction est " + soustraction)
+}
+
+function multiplication(){
+    let n1 = document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1);
+    let numero2 = parseFloat(n2);
+
+    let multiplication = numero1 * numero2
+
+    window.alert("le resultat de la multiplication est " + multiplication)
+}
+
+function division(){
+    let n1 = document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+
+    let numero1 = parseFloat(n1);
+    let numero2 = parseFloat(n2);
+
+    let division = numero1 / numero2
+
+    window.alert("le resultat de la division est " + division)
+}
